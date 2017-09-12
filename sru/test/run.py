ddb780f080a45740497861db41aec95791ca73ae
musyoku <musyoku@users.noreply.github.com>
2017-09-12 15:12:39 +0900

    :v:

--- sru/test/run.py
+++ sru/test/run.py
@@ -1,1 +1,12 @@
-from .. import SRU
+import sys, os
+import numpy as np
+sys.path.append(os.path.join(".."))
+from sru import SRU
+
+def main():
+	data = np.random.normal(0, 1, size=(48, 128, 50)).astype(np.float32)
+	layer = SRU(128, 128)
+	h = layer(data)
+
+if __name__ == "__main__":
+	main()
