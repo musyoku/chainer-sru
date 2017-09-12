f866fb02f85cd473dd13ce9df0427b8e8a24ee46
musyoku <musyoku@users.noreply.github.com>
2017-09-12 16:07:38 +0900

    :v:

--- sru/test/run.py
+++ sru/test/run.py
@@ -3,28 +3,31 @@ import cupy as xp
 from chainer import links
 import torch
 from torch.autograd import Variable
-from cuda_functional import SRU as PyTorchSRU
 sys.path.append(os.path.join(".."))
 from sru import SRU
 
-# @profile
+@profile
 def main():
 	gpu_device = 1
-	with cp.cuda.Device(gpu_device):
+	# Chainer
+	with xp.cuda.Device(gpu_device):
 		data = xp.random.normal(0, 1, size=(48, 128, 50)).astype(xp.float32)
 		# SRU
-		# layer = SRU(128, 128)
-		# for _ in range(10):
-		# 	h = layer(data)
-		# 	layer.reset_state()
-		# # LSTM
-		# layer = links.LSTM(128, 128)
-		# for _ in range(10):
-		# 	for t in range(data.shape[2]):
-		# 		h = layer(data[..., t])
-		# 	layer.reset_state()
-		# SRU (PyTorch)
+		layer = SRU(128, 128)
+		layer.to_gpu(gpu_device)
+		for _ in range(10):
+			h = layer(data)
+			layer.reset_state()
+		# LSTM
+		layer = links.LSTM(128, 128)
+		layer.to_gpu(gpu_device)
+		for _ in range(10):
+			for t in range(data.shape[2]):
+				h = layer(data[..., t])
+			layer.reset_state()
+	# PyTorch
 	with torch.cuda.device(gpu_device):
+		from cuda_functional import SRU as PyTorchSRU
 		rnn = PyTorchSRU(128, 128,
 			num_layers = 1,
 			dropout = 0.0,
@@ -33,7 +36,8 @@ def main():
 			bidirectional = False
 		)
 		rnn.cuda()
-		output, hidden = rnn(Variable(torch.FloatTensor(50, 48, 128).cuda()))
+		for _ in range(10):
+			output, hidden = rnn(Variable(torch.FloatTensor(50, 48, 128).cuda()))
 
 if __name__ == "__main__":
 	main()
